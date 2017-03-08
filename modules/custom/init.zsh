107e6cf845304127ffc427de04d3d1205954d242
Ryan Wood <ryan.wood@gmail.com>
2017-03-08 10:20:22 -0500

    add directory and git aliases

--- modules/custom/init.zsh
+++ modules/custom/init.zsh
@@ -5,4 +5,7 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
-
+alias ...='cd ..\..'
+alias ....='cd ..\..\..'
+alias s='git status'
+alias d='git diff'
